cac63f8bd1bd5d8efd40904be2caff9b6005ce4c
Joshua Horowitz <joshuah@alum.mit.edu>
2017-12-24 02:50:56 -0500

    switch to `element` from `selector`

--- index.js
+++ index.js
@@ -7,25 +7,6 @@ const emitter = new EventEmitter();
 let scriptLoaded = false;
 let scriptLoading = false;
 
-//http://stackoverflow.com/questions/4588119/get-elements-css-selector-when-it-doesnt-have-an-id
-function fullPath(el){
-  var names = [];
-  while (el.parentNode){
-    if (el.id){
-      names.unshift('#'+el.id);
-      break;
-    }else{
-      if (el==el.ownerDocument.documentElement) names.unshift(el.tagName);
-      else{
-        for (var c=1,e=el;e.previousElementSibling;e=e.previousElementSibling,c++);
-        names.unshift(el.tagName+":nth-child("+c+")");
-      }
-      el=el.parentNode;
-    }
-  }
-  return names.join(" > ");
-}
-
 
 class Apparatus extends React.Component {
 
@@ -85,14 +66,14 @@ class Apparatus extends React.Component {
     console.log('initializing viewer')
     console.log({
       url: this.props._url,
-      selector: fullPath(this._ref),
+      element: this._ref,
       regionOfInterest: this.props._regionOfInterest,
       onRender: this.handleViewerRender
     })
     this.setState({
       viewer: new ApparatusViewer({
         url: this.props._url,
-        selector: fullPath(this._ref),
+        element: this._ref,
         regionOfInterest: this.props._regionOfInterest,
         onRender: this.handleViewerRender
       })
